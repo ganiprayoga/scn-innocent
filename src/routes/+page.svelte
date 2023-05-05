0b5bf7acee713725c3a8cfc20ac078a3bf2a138d
Gani Prayoga <gani.prayoga@gmail.com>
2023-05-05 16:43:44 +0700

    Penyesuaian Homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,14 +2,21 @@
 	import SCNLogo from '$lib/assets/Smart City Nusantara - Primary.svg?component';
 </script>
 
-<div class="flex flex-col p-8 min-h-screen gap-4 justify-between">
-	<div class="flex flex-row">
-		<SCNLogo class="h-64 w-64" />
+<div class="flex flex-col min-h-screen gap-4 justify-between bg-black-10">
+	<div class="flex flex-col p-4">
+		<div class="flex flex-row">
+			<SCNLogo class="h-10 w-10" />
+		</div>
 	</div>
-	<div class="flex flex-col">
-		<span class="title-3">Selamat datang di Interplay</span>
-		<h1 class="large-title">Smart City Nusantara</h1>
+	<div class="flex flex-col bottom-0 gap-8 p-8 bg-white">
+		<div class="flex flex-col">
+			<span class="title-3 font-semibold">Selamat datang di Interplay</span>
+			<h1 class="title-1 font-bold">Smart City Nusantara</h1>
+		</div>
+		<p class="body">
+			Kami akan memandu anda untuk memahami konsep Smart City Nusantara dan mencoba pengalaman
+			mengelola kota cerdas.
+		</p>
+		<button class="bg-primary text-white w-full p-3 rounded-md">Sekilas SCN</button>
 	</div>
-	<span>-----</span>
-	<button class="bottom-0 bg-primary text-white w-full p-4 rounded-md">Next</button>
 </div>
