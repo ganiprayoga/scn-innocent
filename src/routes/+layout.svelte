258aab333e18d2433228df3fccfcbb008e809454
Gani Prayoga <gani.prayoga@gmail.com>
2023-05-11 12:34:45 +0700

    Add Page Title

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,10 @@
 	import '$lib/styles/app.scss';
 </script>
 
+<svelte:head>
+	<title>Smart City Nusantara Interplay</title>
+</svelte:head>
+
 <div class="w-full md:max-w-[600px] mx-auto">
 	<slot />
 </div>
