0a31ff63fc240c1c3f68dbc11742c7c1a05deea9
Gani Prayoga <gani.prayoga@gmail.com>
2023-07-07 15:08:20 +0700

    Analytics.

--- src/routes/(mobile)/+layout.svelte
+++ src/routes/(mobile)/+layout.svelte
@@ -1,14 +1,17 @@
 <script lang="ts">
 	import '$lib/styles/app.scss';
 	import Favicon from '$lib/assets/sc-favicon.svg?url';
+	import Analytics from '$lib/analytics.svelte';
 
-	export let data;
+	export let data: any;
 </script>
 
 <svelte:head>
 	<title>Smart City Nusantara Interplay</title>
 	<link rel="icon" href={Favicon} type="image/svg+xml" />
 </svelte:head>
+
+<Analytics />
 
 <div class="w-full md:max-w-[600px] mx-auto overflow-x-hidden">
 	<slot />
